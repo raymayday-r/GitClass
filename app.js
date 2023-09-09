6e848825ab3dc8512744d9ff239996434aa75866
Gunleon <Gunleon@gunmail.com>
2023-09-09 19:48:24 +0800

    Git課程

---
+++ app.js
@@ -0,0 +1,24 @@
+// function sumArray(arr) {
+//   let result = 0;
+//   arr.forEach((element) => {
+//     result += element;
+//   });
+// }
+
+// console.log(sumArray("hello"));
+
+// function sumArray(arr) {
+//   if (!Array.isArray(arr)) {
+//     throw new TypeError("參數並非Array");
+//   }
+//   let result = 0;
+//   arr.forEach((element) => {
+//     result += element;
+//   });
+// }
+
+// try {
+//   sumArray("hello");
+// } catch (e) {
+//   console.log(e);
+// }
